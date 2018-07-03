9308040fcaef41698eb21fef99633469039c5060
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-03 17:45:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Started my service with Shotgun "
+    "Started my service with Shotgun. And I like it. "
   end
 
 end
